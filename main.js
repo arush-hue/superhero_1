62cc24b4f6c34c70dfe2e34378939d8e4e86fb6b
arush-hue <77105133+arush-hue@users.noreply.github.com>
2021-04-29 17:48:27 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var canvas= new fabric.Canvas('myCanvas');
+block_width=30;
+block_height=30;
+player_x=10;
+player_y=10;
+player_object="";
+block_object="";
+function playerUpdate()
+{
+    fabric.Image.fromURL("player.png",function(Img){
+        player_object=Img;
+        player_object.scaleToWidth(150);
+        player_object.scaleToHeight(140);
+        player_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(player_object);
+    });
+}
+function newImage(get_image)
+{
+    fabric.Image.fromURL(get_image,function(Img){
+        block_object=Img;
+        block_object.scaleToWidth(block_width);
+        block_object.scaleToHeight(block_height);
+        block_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(block_object);
+    });
+}
